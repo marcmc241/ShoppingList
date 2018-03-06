36903284c611fa27e5bb55fd77ce5f019ada3951
marcmc241 <marcmc241@gmail.com>
2018-03-06 10:34:36 +0100

    Layout fet.

---
+++ app/src/main/java/masip/marc/shoppinglist/activity_shopping_list.java
@@ -0,0 +1,13 @@
+package masip.marc.shoppinglist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_shopping_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+    }
+}
