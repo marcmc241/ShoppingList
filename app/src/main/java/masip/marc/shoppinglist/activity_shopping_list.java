7a7e5dc70988b7de433242dfd88552500f3a42c3
marcmc241 <marcmc241@gmail.com>
2018-03-06 11:17:59 +0100

    Mostrem una llista d'elements ficticis

--- app/src/main/java/masip/marc/shoppinglist/activity_shopping_list.java
+++ app/src/main/java/masip/marc/shoppinglist/activity_shopping_list.java
@@ -2,12 +2,31 @@ package masip.marc.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class activity_shopping_list extends AppCompatActivity {
+
+    private ListView list;
+    private ArrayList<String> items; //model de dades
+    private ShoppingListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        //omplim el model de dades
+        items = new ArrayList<>();
+        items.add("Patates");
+        items.add("Paper WC");
+        items.add("Ketchup");
+
+        list = (ListView) findViewById(R.id.list);
+
+        adapter = new ShoppingListAdapter(this, R.layout.shopping_item, items);
+
+        list.setAdapter(adapter);
     }
 }
